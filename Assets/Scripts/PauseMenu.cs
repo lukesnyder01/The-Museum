7c0af7ade65ae99869a6115f7c5f2b3b39e0be81
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2025-02-02 07:36:48 -0800

    build: import project files

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused;
+
+    public GameObject pauseMenuUI;
+
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenuUI.SetActive(false);
+        gameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        pauseMenuUI.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,177 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    [Header("Player Settings")]
+
+    public float walkSpeed;
+    public float runSpeed;
+    public bool isRunning;
+
+    public float jumpForce;
+
+    public float gravity;
+    public float lateralSprintSpeedPenalty = 0.75f;
+
+
+    [Header("References")]
+
+    public Transform headCheckPosition;
+    public Transform groundCheckPosition;
+
+    private CharacterController characterController;
+    private Transform cameraTransform;
+
+
+    [Header("Other")]
+
+    public float groundCheckRadius;
+    public float headCheckRadius;
+    public LayerMask groundMask;
+
+    //Private variables
+
+    private float xMove;
+    private float zMove;
+
+    private Vector3 moveDirection;
+
+    private float moveSpeed;
+    private bool isGrounded;
+
+    private bool hitHead;
+    private Vector3 velocity;
+
+
+
+
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
+        Screen.lockCursor = true;
+    }
+
+
+    void Update()
+    {
+        
+
+        xMove = Input.GetAxis("Horizontal");
+        zMove = Input.GetAxis("Vertical");
+
+
+        SetPlayerMoveSpeed();
+
+
+        isGrounded = Physics.CheckSphere(groundCheckPosition.position, groundCheckRadius, groundMask);
+
+        hitHead = Physics.CheckSphere(headCheckPosition.position, headCheckRadius, groundMask);
+
+
+        if (isGrounded)
+        {
+
+            PlayerHitsGround();
+
+            characterController.stepOffset = 0.5f;          //allows player to climb stairs
+        }
+        else
+        {
+
+        }
+
+
+
+        moveDirection = transform.right * xMove * moveSpeed * lateralSprintSpeedPenalty + transform.forward * zMove * moveSpeed;
+
+
+
+
+        if (hitHead && velocity.y > 0)
+        {
+            velocity.y = -0.1f;
+        }
+
+        if (!isGrounded)
+        {
+            characterController.stepOffset = 0.0001f;       //prevents player from catching on edges when jumping up near them
+        }
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Debug.Log("Tried to jump");
+            velocity.y += jumpForce;
+        }
+
+
+        //clamp maximum vertical speed from jumping
+        if (velocity.y > jumpForce)
+        {
+            velocity.y = jumpForce;
+        }
+
+
+        velocity.y += gravity * Time.deltaTime;
+
+
+        characterController.Move(moveDirection * Time.deltaTime + velocity * Time.deltaTime);
+
+
+
+
+
+    }
+
+
+
+    void PlayerHitsGround()
+    {
+
+        if (velocity.y < 0f)                                //slowly push player down so they keep in contact with the ground
+        {
+            velocity.y = -2f;
+        }
+    }
+
+
+    void SetPlayerMoveSpeed()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && zMove == 1)
+        {
+            moveSpeed = runSpeed;
+            isRunning = true;
+        }
+        else
+        {
+            moveSpeed = walkSpeed;
+            isRunning = false;
+        }
+    }
+
+
+
+    /*
+    void PlayFootsteps()
+    {
+        if (moveDirection != new Vector3(0f, 0f, 0f) && Mathf.Abs(velocity.y) < 0.52)
+        {
+            nextFootStep -= Time.deltaTime;
+
+            if (nextFootStep <= 0f)
+            {
+                audioManager.Play("PlayerStep");
+                nextFootStep += footStepDelay + 0.25f / moveSpeed;
+            }
+        }
+    }
+    */
+
+}
