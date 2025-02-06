1ff9861a13c2e7c5bce10240cc836f77e4a912fd
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2025-02-06 10:00:25 -0800

    gameplay: add pressable button that rotates painting

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour, IInteractable
+{
+    public GameObject target;
+    private IActivatable targetActivatable;
+
+    private Vector3 depressTargetPos;
+    private float pressDistance = 0.05f;
+    private float returnTime = 0.1f;
+    private Vector3 startPosition;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        if (target.TryGetComponent<IActivatable>(out IActivatable activatable))
+        {
+            targetActivatable = activatable;
+        }
+
+        startPosition = transform.position;
+        depressTargetPos = startPosition + -transform.up * pressDistance;
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+
+    public void Interact()
+    {
+        Debug.Log("Pressed Button");
+        StartCoroutine(DepressButton());
+        audioSource.Play();
+
+        if (targetActivatable != null)
+        {
+            targetActivatable.Activate();
+        }
+
+
+    }
+
+
+    IEnumerator DepressButton()
+    {
+        transform.position = depressTargetPos;
+        yield return new WaitForSeconds(returnTime);
+        transform.position = startPosition;
+        yield return null;
+    }
+
+    
+
+}
---
+++ Assets/Scripts/RotatablePainting.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatablePainting: MonoBehaviour, IActivatable
+{
+    private float rotationSpeed = 30f;
+
+    private Vector3 startingUpVector;
+
+    private bool isRotating = false;
+
+    private Quaternion targetRotation;
+
+    void Start()
+    {
+        targetRotation = transform.rotation;
+    }
+
+    public void Activate()
+    {
+        if (!isRotating)
+        {
+            StartCoroutine(RotateSmoothly());
+        }
+    }
+
+    private IEnumerator RotateSmoothly()
+    {
+        isRotating = true;
+
+        targetRotation *= Quaternion.Euler(0, 0, 90);
+
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.01f)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.rotation = targetRotation; // Snap to targetRotation
+
+        isRotating = false;
+    }
+
+}
