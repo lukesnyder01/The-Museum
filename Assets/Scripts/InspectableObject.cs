73f0649c4e9e2f9e458f1798aaf50ec31fc2a501
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2025-02-20 07:38:49 -0800

    asset: add new art prefabs

---
+++ Assets/Scripts/InspectableObject.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+
+public class InspectableObject : MonoBehaviour, IInteractable
+{
+    public float inspectDistance = 0.5f;
+
+    private float rotationSpeed = 200;
+
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+    private bool isBeingInspected = false;
+    private Transform cameraTransform;
+
+    private PlayerController playerController;
+    private SimpleSmoothMouseLook playerMouseLook;
+
+    private GameObject canvas;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        playerMouseLook = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<SimpleSmoothMouseLook>();
+
+        // Cache the start position and rotation
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+
+        cameraTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
+        // Move the object to its start position
+        // to try and compile the shader variant
+        transform.position = startPosition;
+        transform.rotation = startRotation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingInspected)
+        {
+            HandleRotation();
+        }
+    }
+
+    public void Interact()
+    {
+        if (!isBeingInspected)
+        {
+            StartInspecting();
+        }
+        else
+        {
+            StopInspecting();
+        }
+    }
+
+    void StartInspecting()
+    {
+        isBeingInspected = true;
+
+        canvas.SetActive(false);
+
+        playerController.movementLocked = true;
+        playerMouseLook.mouseLookLocked = true;
+
+        transform.position = cameraTransform.position + cameraTransform.forward * inspectDistance;
+        transform.rotation = cameraTransform.rotation;
+    }
+
+    void StopInspecting()
+    {
+        isBeingInspected = false;
+
+        canvas.SetActive(true);
+
+        playerController.movementLocked = false;
+        playerMouseLook.mouseLookLocked = false;
+
+        transform.position = startPosition;
+        transform.rotation = startRotation;
+    }
+
+    void HandleRotation()
+    {
+        // Get mouse movement on the X-axis
+        float mouseX = Input.GetAxis("Mouse X");
+
+        // Rotate the object around its Y-axis
+        transform.Rotate(transform.up, -mouseX * rotationSpeed * Time.deltaTime, Space.World);
+    }
+
+}
